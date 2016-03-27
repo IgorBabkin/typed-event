ea1617de0602b6e49ab055d6406fe30b7cf3d541
IgorBabkin <igba14@gmail.com>
2016-03-27 17:23:20 +0300

    functionality is complete

--- src/typed_event.ts
+++ src/typed_event.ts
@@ -1,3 +1,29 @@
-export default class TypedEvent {
+export interface ISubscriber<G> {
+    (data:G):void;
+}
 
-} 
+export default class TypedEvent<T> {
+    private subscribers: ISubscriber<T>[] = [];
+
+    subscribe(handler:ISubscriber<T>):void {
+        if (!this.has(handler)) {
+            this.subscribers = this.subscribers.concat([handler]);
+        }
+    }
+
+    unsubscribe(handler:ISubscriber<T>):void {
+        this.subscribers = this.subscribers.filter((subscriber: ISubscriber<T>) => {
+            return handler !== subscriber;
+        });
+    }
+
+    raise(data?:T):void {
+        this.subscribers.forEach((subscriber: ISubscriber<T>) => {
+            subscriber(data);
+        })
+    }
+
+    private has(subscriber:ISubscriber<T>): boolean {
+        return this.subscribers.indexOf(subscriber) !== -1;
+    }
+}
